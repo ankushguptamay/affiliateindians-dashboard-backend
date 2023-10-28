98cb45a997846ffef8559860a9b8207ab87d9d27
Ankush Gupta <ankushgupta9675@gmail.com>
2023-10-28 16:22:11 +0530

    User signin and signup done

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
-const admin = require('./Routes/Admin/adminRoute')
-const superAdmin = require('./Routes/Admin/superAdminRoute')
+const admin = require('./Routes/Admin/adminRoute');
+const superAdmin = require('./Routes/Admin/superAdminRoute');
+const user = require('./Routes/User/userRoute');
 
 const app = express();
 
@@ -29,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/admin", admin);
 app.use("/api/superAdmin", superAdmin);
+app.use("/api/user", user);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
