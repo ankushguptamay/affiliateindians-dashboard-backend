21e8f89aa5b5e9c5806bf7bcdc3d700e9db9fdb6
Ankush Gupta <ankushgupta9675@gmail.com>
2023-10-28 16:24:14 +0530

    User signin and signup done

--- Routes/User/userRoute.js
+++ Routes/User/userRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const { body } = require('express-validator');
 
 const { create, changePassword, login, findUser } = require("../../Controllers/User/user");
 
